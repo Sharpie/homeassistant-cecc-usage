3c6690f27495c5897415901d898d568fa64299aa
Charlie Sharpsteen <source@sharpsteen.net>
2024-05-24 08:44:02 -0400

    Add account number validation
    This commit adds validation of account numbers to `config_flow.py`.
    This integration will require the account number, not a user name,
    as the usage data API requires it as an input.

--- custom_components/cecc_usage/config_flow.py
+++ custom_components/cecc_usage/config_flow.py
@@ -14,11 +14,20 @@ class CeccUsageConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     MINOR_VERSION = 1
 
+    async def _validate_user_input(self, user_input: dict, errors: dict):
+        try:
+            vol.Match(r'\d+-\d+')(user_input[CONF_USERNAME])
+        except vol.MatchInvalid:
+            errors[CONF_USERNAME] = "invalid_account_number"
+
     async def async_step_user(self, user_input: dict|None =None) -> config_entries.FlowResult:
         errors = {}
 
         if user_input is not None:
-            # TODO: Input validation.
-            return self.async_create_entry(title=user_input[CONF_USERNAME], data=user_input)
+            await self._validate_user_input(user_input, errors)
+
+            if not errors:
+                return self.async_create_entry(title=user_input[CONF_USERNAME], data=user_input)
 
         return self.async_show_form(step_id="user", data_schema=USER_SCHEMA, errors=errors)
+
